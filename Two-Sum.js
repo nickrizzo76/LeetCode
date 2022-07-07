504dcd46ada8c716c4333547502c09ce1fe52180
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-07 14:34:15 -0400

    Update Two-Sum.js
    Add 2nd attempt

--- Two-Sum.js
+++ Two-Sum.js
@@ -1,16 +1,41 @@
 / **
   * URL: https://leetcode.com/problems/two-sum
-  * Description: A naive solution to the problem under time constraint.
-  *
+  * 
   *
 /
 
+// Attempt 1: naive solution
 var twoSum = function(nums, target) {
     for(let i = 0; i < nums.length; i++) {
         for(let j = i+1; j < nums.length; j++) {
             if(nums[i] + nums[j] === target) {
                 return [i,j]
             }
+        }
+    }
+};
+
+// Attempt 2: solve with dictionary - still inefficient (has roughly the same performance as Attempt 1)
+var twoSum = function(nums, target) {
+    const dict = {}
+    nums.forEach((num, index) => {
+        if(dict[num]) dict[num] = [...dict[num], index]
+        else dict[num] = [index];
+    })
+    
+    
+    const keys = Object.keys(dict)
+    
+    for(let i = 0; i < keys.length; i++) {
+        const currentKey = keys[i];
+        const keyToSearchFor = target - currentKey;
+        
+        if(keyToSearchFor == currentKey) {
+            return dict[currentKey];
+        }
+        
+        if(dict[keyToSearchFor]) {
+            return [dict[currentKey], dict[keyToSearchFor]]
         }
     }
 };
