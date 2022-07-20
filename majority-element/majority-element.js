7bc0baceb7e8a37673c62e68f5a5c16f20758003
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-20 10:54:54 -0400

    Update majority-element.js

--- majority-element/majority-element.js
+++ majority-element/majority-element.js
@@ -3,6 +3,8 @@
  * @return {number}
  */
 
+// Hashmap Solution
+// O(n) Time and Space Complexity
 var majorityElement = function(nums) {
     const dict = {}
     let maxCount = Number.NEGATIVE_INFINITY;
@@ -21,3 +23,19 @@ var majorityElement = function(nums) {
     }
     return maxElement;
 };
+
+// Boyer-Moore Voting Algorithm
+// first try from memory of how the algorithm works
+let maxElement = nums[0];
+    let count = 1;
+    for(let i = 1; i < nums.length; i++) {
+        if(count === 0) {
+            maxElement = nums[i];
+            count = 1;
+        } else if(nums[i] === maxElement) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    return maxElement;
