698b76b8f5ce66129ddd3b8ff03bc35a1fbe9d1c
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-20 10:57:32 -0400

    Update majority-element.js

--- majority-element/majority-element.js
+++ majority-element/majority-element.js
@@ -27,9 +27,27 @@ var majorityElement = function(nums) {
 // Boyer-Moore Voting Algorithm
 // O(n) Time + O(1) Space Complexity
 // first try from memory of how the algorithm works
-let maxElement = nums[0];
-    let count = 1;
-    for(let i = 1; i < nums.length; i++) {
+var majorityElement = function(nums) {
+    let maxElement = nums[0];
+        let count = 1;
+        for(let i = 1; i < nums.length; i++) {
+            if(count === 0) {
+                maxElement = nums[i];
+                count = 1;
+            } else if(nums[i] === maxElement) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return maxElement;
+}
+
+// set local vars to default values instead of pre-emptively setting them to the first element in *nums* and starting the loop 1 index later.
+var majorityElement = function(nums) {
+    let maxElement = null;
+    let count = 0;
+    for(let i = 0; i < nums.length; i++) {
         if(count === 0) {
             maxElement = nums[i];
             count = 1;
@@ -40,3 +58,4 @@ let maxElement = nums[0];
         }
     }
     return maxElement;
+};
