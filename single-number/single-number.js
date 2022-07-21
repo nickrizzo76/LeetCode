46994eed748697531ed103db8ea8a587a1959e49
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-20 22:54:41 -0400

    Create single-number.js

---
+++ single-number/single-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    const set = new Set();
+    
+    nums.forEach(num => {
+        if(set.has(num)) {
+            set.delete(num)
+        } else {
+            set.add(num)
+        }
+    })
+    return Array.from(set)[0];
+};
