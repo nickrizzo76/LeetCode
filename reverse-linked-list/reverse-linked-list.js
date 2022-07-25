191d46f50ffd69ac2ea04d92a2a4e4ecf006b105
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-25 15:06:03 -0400

    Update reverse-linked-list.js

--- reverse-linked-list/reverse-linked-list.js
+++ reverse-linked-list/reverse-linked-list.js
@@ -10,6 +10,7 @@
  * @return {ListNode}
  */
 
+// SOLUTION with a Stack
 var reverseList = function(head) {
     // return input if empty
     if(!head) return head;
@@ -37,4 +38,41 @@ var reverseList = function(head) {
   
     return head;
     
+};
+
+
+// Solution: Reverse in place
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    // return input if empty
+    if(!head) return head;
+    
+    // set 1st and 2nd pointer
+    let previous = head;
+    let current = previous.next;
+    // remove circular reference. This is going to be the last node in the reversed linkedlist, so its 'next' should be null.
+    previous.next = null;
+    
+    while(current) {
+        // save reference to the next node
+        const next = current.next;
+        // reverse the link
+        current.next = previous;
+        // iterate previous
+        previous = current;
+        // iterate current
+        current = next;
+    }
+    
+    return previous;
 };
