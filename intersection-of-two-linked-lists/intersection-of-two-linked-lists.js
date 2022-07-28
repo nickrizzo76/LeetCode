931083c219c2d5f719b6f8674c336fa8311fe284
Nick Rizzo <91131873+nickrizzo76@users.noreply.github.com>
2022-07-28 16:19:09 -0400

    Update intersection-of-two-linked-lists.js

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.js
@@ -11,6 +11,8 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
+
+// The naive, dictionary approach that doesn't take advantage of the LinkedList data structure
 var getIntersectionNode = function(headA, headB) {
     const nodes = new Set();
     
@@ -31,3 +33,36 @@ var getIntersectionNode = function(headA, headB) {
     }
     return null;
 };
+
+
+// Solution that solves the problem by the fact that both linked lists are guaranteed not to have negative numbers in them.  Knowing that, we can negate all the nodes' values in one list, then search for any negative numbers in the other list.
+var getIntersectionNode = function(headA, headB) {
+    negateList(headA)
+    
+    let nodeB = headB;
+    let result = null;
+    
+    // search for a negative node value
+    while(nodeB) {
+        if(nodeB.val < 0) {
+            // store node with negative value and stop searching
+            result = nodeB;
+            break;
+        }
+        nodeB = nodeB.next;
+    }
+    // restore headA back to its original form
+    negateList(headA)
+    
+    return result;
+};
+
+function negateList(nodeA) {
+    const head = nodeA
+    while(nodeA) {
+        nodeA.val = -nodeA.val
+        nodeA = nodeA.next;
+    }
+    
+    return head;
+}
